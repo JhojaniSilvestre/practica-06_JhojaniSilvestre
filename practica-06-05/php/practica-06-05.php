e5c559da79bbf109048c7c7e6213e46d40b09061
JhojaniSilvestre <maellysilvestre@gmail.com>
2022-05-23 13:55:33 +0200

    ejercicio 5 ver 2

--- practica-06-05/php/practica-06-05.php
+++ practica-06-05/php/practica-06-05.php
@@ -6,20 +6,29 @@
     $a = $valores['a'];
 	$b = $valores['b'];
     $c = $valores['c'];
-    
-    $neg = -1; 
-    
-    $menosb = $b * $neg; 
-    $oper1 = pow($b,2); //pow() el valor de base elevado a la potencia
-    $oper2 = 4*$a*$c; 
+    //resuelvo la -b
+    $menosb = -1 * $b;
+    //resuelvo primero la potencia
+    $oper1= pow($b,2);
+    //luego las multiplicaciones
+    $oper2 = 4*$a*$c;
+    //el resulde la potencia - el resul de la multi
     $resta = $oper1-$oper2; 
-    $raiz = pow($resta,(1/2)); 
-    $dosa = 2*$a; 
-    
-    $result1 = ($menosb + $raiz)/$dosa; 
-    $result2 = ($menosb - $raiz)/$dosa; 
-    
-/*------------------------------------------------*/
+    //compruebo que la resta no es menor que 0
+    if ($resta < 0) {
+        $result1 = "raiz negativa, no existe"; 
+        $result2 = "raiz negativa, no existe";
+    }
+    else {
+        //obtengo la raiz cuadrada de la resta
+        $raiz = sqrt($resta);
+        //resuelvo 2 * a
+        $dosa = 2*$a;
+        //obtengo los dos resultados diferentes
+        $result1 = ($menosb + $raiz)/$dosa; 
+        $result2 = ($menosb - $raiz)/$dosa;
+    }    
+    //una vez resuelto, asigno cada resultado a los atributos de la clase
 	$nuevo= new stdClass();
 	$nuevo->solucion1=$result1;
 	$nuevo->solucion2=$result2;
